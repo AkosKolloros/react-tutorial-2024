1082737c454a44f4b527d670a9f3111a78c80002
Akos Kolloros <kollorosa@gmail.com>
2024-09-05 03:10:27 +0200

    userGreeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Student from "./StudentModule/Student";
+import UserGreeting from "./UserGreetingModule/UserGreeting";
 
 function App() {
   return (
     <>
-      <Student name="SpongeBob" age={30} isStudent={true}></Student>
-      <Student name="Patrick" age={42} isStudent={false}></Student>
-      <Student name="Squidward" age={50} isStudent={false}></Student>
-      <Student name="Sandy" age={27} isStudent={true}></Student>
-      <Student></Student>
+      <UserGreeting isLoggedIn={true} username="Akos"></UserGreeting>
     </>
   );
 }
